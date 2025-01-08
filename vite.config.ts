a40cd6c515edd8e03fd9ab7bac7a9e981d56c6d1
Diego Pinilla <dpinilla@sistemaenlinea.com.co>
2025-01-08 09:21:32 -0500

    fix: configure ESLint and TypeScript, add aliases in Vite and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vitesconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitesconfigPaths()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@ptypes": path.resolve(__dirname, "./src/types"),
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@context": path.resolve(__dirname, "./src/context"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@design": path.resolve(__dirname, "./src/design"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+    },
+  },
 });
