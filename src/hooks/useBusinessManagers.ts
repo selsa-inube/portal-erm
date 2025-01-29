add4dc745527083019f7740a4496fef1ee307dad
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2025-01-29 08:01:51 -0500

    refactor(useBusinessManagers): add flag in error

--- src/hooks/useBusinessManagers.ts
+++ src/hooks/useBusinessManagers.ts
@@ -6,6 +6,8 @@ import {
 } from "@src/types/employeePortalBusiness.types";
 import { getBusinessManagerById } from "@services/businessManagers/getBusinessManagerById";
 
+import { useErrorFlag } from "./useErrorFlag";
+
 export const useBusinessManagers = (
   portalPublicCode: IEmployeePortalByBusinessManager,
 ) => {
@@ -13,11 +15,16 @@ export const useBusinessManagers = (
     useState<IBusinessManager>({} as IBusinessManager);
   const [hasError, setHasError] = useState(false);
   const [codeError, setCodeError] = useState<number | undefined>(undefined);
+  const [isFetching, setIsFetching] = useState(true);
+  const [flagShown, setFlagShown] = useState(false);
+
+  useErrorFlag(flagShown);
 
   useEffect(() => {
     const fetchBusinessManagers = async () => {
       if (!portalPublicCode?.businessManagerId) return;
 
+      setIsFetching(true);
       try {
         const fetchedBusinessManagers = await getBusinessManagerById(
           portalPublicCode.businessManagerId,
@@ -29,9 +36,11 @@ export const useBusinessManagers = (
         ) {
           setHasError(true);
           setCodeError(1006);
+          setFlagShown(true);
           return;
         }
 
+        setHasError(false);
         setBusinessManagersData(fetchedBusinessManagers);
       } catch (err) {
         console.error(
@@ -40,11 +49,14 @@ export const useBusinessManagers = (
         );
         setHasError(true);
         setCodeError(1007);
+        setFlagShown(true);
+      } finally {
+        setIsFetching(false);
       }
     };
 
     fetchBusinessManagers();
   }, [portalPublicCode]);
 
-  return { businessManagersData, hasError, codeError };
+  return { businessManagersData, hasError, codeError, isFetching };
 };
