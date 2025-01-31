b7074b8489d0a95401dadb1d668e9edea2e7602a
Angelbt12 <125980817+Angelbt12@users.noreply.github.com>
2025-01-31 15:26:35 -0500

    at/erm26/typescript-compilation-error (#8)

---
+++ src/components/layout/AppPage/config.ts
@@ -0,0 +1,12 @@
+import { IStaffPortalByBusinessManager } from "@ptypes/staffPortalBusiness.types";
+
+export const mockDataPortal: IStaffPortalByBusinessManager = {
+  abbreviatedName: "Test Name",
+  businessManagerId: "12345",
+  descriptionUse: "Test description",
+  optionsByStaffPortalBusinessManager: [],
+  publicCode: "ABC123",
+  staffPortalCatalogId: "SPC123",
+  staffPortalId: "SP123",
+  url: "https://example.com",
+};
