faf07dfe0ddb61fe4bc5927c4cba2d7068283064
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2025-03-06 12:42:52 -0500

    feat(contractualPositionDataForm): employee onboarding assistant contractual position step

---
+++ src/pages/Employees/NewEmployee/forms/ContractualPositionDataForm/config/formConfig.ts
@@ -0,0 +1,14 @@
+const contractualPositionDataRequiredFields = {
+  normativeFramework: true,
+  contractType: true,
+  startDate: true,
+  endDate: true,
+  company: true,
+  workingShift: true,
+  team: true,
+  position: true,
+  salaryProfile: true,
+  jobMode: true,
+};
+
+export { contractualPositionDataRequiredFields };
---
+++ src/pages/Employees/NewEmployee/forms/ContractualPositionDataForm/styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { inube } from "@inubekit/inubekit";
+
+import { spacing } from "@design/tokens/spacing";
+
+interface StyledContainerProps {
+  $isMobile: boolean;
+  theme?: typeof inube;
+}
+
+const StyledContainer = styled.div<StyledContainerProps>`
+  display: flex;
+  flex-direction: column;
+  gap: ${spacing.s250};
+  border-radius: ${spacing?.s100};
+  border: 1px solid
+    ${({ theme }) => theme?.palette?.neutral?.N30 || inube.palette.neutral.N30};
+  padding: ${({ $isMobile }) =>
+    $isMobile ? `${spacing?.s300} ${spacing?.s150}` : spacing?.s300};
+
+  textarea {
+    resize: none;
+  }
+`;
+
+export { StyledContainer };
