64194d77ce9c5daf0611950bbfc204a5514a5893
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2025-03-10 08:29:53 -0500

    fix: component adjustment

--- src/pages/certifications/NewCertification/index.tsx
+++ src/pages/certifications/NewCertification/index.tsx
@@ -1,12 +1,18 @@
 import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { FormikProps } from "formik";
+
+import { SendRequestModal } from "@components/modals/SendRequestModal";
 
 import { newCCertificationApplication } from "./config/assisted.config";
 import { certificationsNavConfig } from "../config/nav.config";
 import { NewCertificationUI } from "./interface";
 import { IGeneralInformationEntry } from "./forms/GeneralInformationForm/types";
+import { RequestInfoModal } from "./modals/RequestInfoModal";
+import { ModalState } from "./types";
 
 function NewCertification() {
+  const navigate = useNavigate();
   const [currentStep, setCurrentStep] = useState(1);
 
   const [formValues, setFormValues] = useState<IGeneralInformationEntry>({
@@ -22,6 +28,11 @@ function NewCertification() {
     useRef<FormikProps<IGeneralInformationEntry>>(null);
 
   const [isCurrentFormValid, setIsCurrentFormValid] = useState(false);
+
+  const [modalState, setModalState] = useState<ModalState>({
+    isSendModalVisible: false,
+    isRequestInfoModalVisible: false,
+  });
 
   const handleNextStep = () => {
     if (currentStep < newCCertificationApplication.length) {
@@ -40,25 +51,60 @@ function NewCertification() {
   };
 
   const handleFinishAssisted = () => {
-    console.log("Proceso de certificación completado");
+    setModalState((prev) => ({ ...prev, isSendModalVisible: true }));
+  };
+
+  const handleCloseSendModal = () => {
+    setModalState((prev) => ({ ...prev, isSendModalVisible: false }));
+  };
+
+  const handleConfirmSendModal = () => {
+    setModalState({
+      isSendModalVisible: false,
+      isRequestInfoModalVisible: true,
+    });
+  };
+
+  const handleSubmitRequestInfoModal = () => {
+    setModalState((prev) => ({ ...prev, isRequestInfoModalVisible: false }));
+    navigate("/certifications");
   };
 
   return (
-    <NewCertificationUI
-      appName={certificationsNavConfig[1].label}
-      appRoute={certificationsNavConfig[1].crumbs}
-      navigatePage={certificationsNavConfig[1].url}
-      steps={newCCertificationApplication}
-      currentStep={currentStep}
-      setCurrentStep={setCurrentStep}
-      handleNextStep={handleNextStep}
-      handlePreviousStep={handlePreviousStep}
-      handleFinishAssisted={handleFinishAssisted}
-      setIsCurrentFormValid={setIsCurrentFormValid}
-      isCurrentFormValid={isCurrentFormValid}
-      generalInformationRef={generalInformationRef}
-      initialGeneralInformationValues={formValues}
-    />
+    <>
+      <NewCertificationUI
+        appName={certificationsNavConfig[1].label}
+        appRoute={certificationsNavConfig[1].crumbs}
+        navigatePage={certificationsNavConfig[1].url}
+        steps={newCCertificationApplication}
+        currentStep={currentStep}
+        setCurrentStep={setCurrentStep}
+        handleNextStep={handleNextStep}
+        handlePreviousStep={handlePreviousStep}
+        handleFinishAssisted={handleFinishAssisted}
+        setIsCurrentFormValid={setIsCurrentFormValid}
+        isCurrentFormValid={isCurrentFormValid}
+        generalInformationRef={generalInformationRef}
+        initialGeneralInformationValues={formValues}
+      />
+      {modalState.isSendModalVisible && (
+        <SendRequestModal
+          descriptionText="¿Realmente deseas enviar la solicitud de certificación?"
+          onSubmitButtonClick={handleConfirmSendModal}
+          onCloseModal={handleCloseSendModal}
+          onSecondaryButtonClick={handleCloseSendModal}
+        />
+      )}
+
+      {modalState.isRequestInfoModalVisible && (
+        <RequestInfoModal
+          requestId="#45678822"
+          staffName="Nombre Nombre Apellido Apellido"
+          onCloseModal={handleSubmitRequestInfoModal}
+          onSubmitButtonClick={handleSubmitRequestInfoModal}
+        />
+      )}
+    </>
   );
 }
 
