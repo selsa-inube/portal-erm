dcba9b04d00aaa4ab2f64423feda0b121ca5bb5d
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2025-03-10 07:27:37 -0500

    fix: conflict adjustment

--- src/validations/validationRules.ts
+++ src/validations/validationRules.ts
@@ -1,5 +1,4 @@
 import * as Yup from "yup";
-
 import { validationMessages } from "./validationMessages";
 
 const validationRules = {
@@ -26,6 +25,22 @@ const validationRules = {
   contract: Yup.string()
     .required(validationMessages.required)
     .max(100, validationMessages.maxCharacters(100)),
+
+  names: Yup.string()
+    .matches(/^[a-zA-Z\s]+$/, validationMessages.onlyLetters)
+    .required(validationMessages.required)
+    .max(50, validationMessages.maxCharacters(50)),
+
+  lastNames: Yup.string()
+    .matches(/^[a-zA-Z\s]+$/, validationMessages.onlyLetters)
+    .required(validationMessages.required)
+    .max(50, validationMessages.maxCharacters(50)),
+
+  identificationNumber: Yup.string()
+    .matches(/^[0-9]+$/, validationMessages.onlyNumbers)
+    .required(validationMessages.required)
+    .min(6, validationMessages.minNumbers(6))
+    .max(15, validationMessages.maxNumbers(15)),
 };
 
 export { validationRules };
