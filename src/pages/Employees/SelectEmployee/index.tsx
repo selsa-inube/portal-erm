6394ca0fa9087d2ee2c7d348c87273b593ace52c
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2025-03-17 15:52:54 -0500

    fix: search engine implementation

--- src/pages/Employees/SelectEmployee/index.tsx
+++ src/pages/Employees/SelectEmployee/index.tsx
@@ -1,175 +1,232 @@
-import { useEffect } from "react";
-import { Text, Button, Textfield, Stack } from "@inubekit/inubekit";
-import { MdOutlineAdd } from "react-icons/md";
-import { IoMdCloseCircleOutline } from "react-icons/io";
+import { useState } from "react";
+import {
+  Text,
+  Button,
+  Stack,
+  Autosuggest,
+  Spinner,
+  useMediaQuery,
+} from "@inubekit/inubekit";
+import { MdOutlineAdd, MdOutlineArrowForward } from "react-icons/md";
 import { Formik, FormikProps } from "formik";
 import * as Yup from "yup";
 import { spacing } from "@design/tokens/spacing";
-import {
-  StyledAppPage,
-  StyledQuickAccessContainer,
-  StyledDropdownMenu,
-  StyledDropdownItem,
-} from "./styles";
-import { useState } from "react";
+import { StyledAppPage, StyledQuickAccessContainer } from "./styles";
 import useAllEmployees from "@hooks/useEmployeeConsultation";
-import useEmployeeById from "@hooks/useEmployeeConsultation";
 
 function SelectEmployeePage() {
-  const { employees, loading, error, refetch } = useAllEmployees();
-  const [filteredEmployees, setFilteredEmployees] = useState(employees);
+  const { employees, loading, error } = useAllEmployees();
+  const [selectedOption, setSelectedOption] = useState("");
+  const isMobile = useMediaQuery("(max-width: 700px)");
 
-  useEffect(() => {
-    setFilteredEmployees(employees);
-  }, [employees]);
+  const employeeOptions = employees.map((emp) => ({
+    id: emp.employeeId,
+    label: `${emp.identificationDocumentNumber} - ${emp.names}`,
+    value: `${emp.identificationDocumentNumber} - ${emp.names}`,
+  }));
 
   const validationSchema = Yup.object({
-    keyword: Yup.string().required(
-      "Para continuar, primero debes seleccionar un empleado.",
-    ),
+    employee: Yup.string()
+      .required("Para continuar, primero debes seleccionar un empleado.")
+      .oneOf(
+        employeeOptions.map((opt) => opt.value),
+        "Debes seleccionar una opción válida.",
+      ),
   });
-
-  const handleSearch = (keyword: string) => {
-    if (!keyword.trim()) {
-      setFilteredEmployees(employees);
-      return;
-    }
-    const results = employees.filter(
-      (emp) =>
-        emp.names.toLowerCase().includes(keyword.toLowerCase()) ||
-        emp.identificationDocumentNumber.includes(keyword),
-    );
-    setFilteredEmployees(results);
-  };
 
   return (
     <Formik
-      initialValues={{ keyword: "" }}
+      initialValues={{ employee: "" }}
       validationSchema={validationSchema}
       onSubmit={(values) => {
-        const { employees, loading, error } = useEmployeeById(
-          Number(values.keyword),
+        const selectedEmployee = employeeOptions.find(
+          (opt) => opt.value === values.employee,
         );
-        if (loading) console.log("Cargando datos del empleado...");
-        if (error) console.error("Error: ", error);
-        if (employees) console.log("Empleado encontrado: ", employees);
+        if (selectedEmployee) {
+          console.log("Empleado seleccionado:", selectedEmployee);
+        } else {
+          console.error("Empleado no encontrado");
+        }
       }}
     >
-      {(formik: FormikProps<{ keyword: string }>) => (
+      {(formik: FormikProps<{ employee: string }>) => (
         <StyledAppPage>
           <Stack direction="column" gap={spacing.s250}>
-            <StyledQuickAccessContainer>
-              <Stack direction="column" gap={spacing.s250}>
+            {!isMobile ? (
+              <StyledQuickAccessContainer>
+                <Stack direction="column" gap={spacing.s250}>
+                  <Text type="headline" size="large">
+                    Seleccionar empleado
+                  </Text>
+
+                  <Text type="body" size="large" appearance="gray">
+                    Digita el nombre del empleado que quieres seleccionar.
+                  </Text>
+
+                  <StyledQuickAccessContainer>
+                    <form onSubmit={formik.handleSubmit}>
+                      <Stack
+                        gap={spacing.s150}
+                        alignItems="center"
+                        width={isMobile ? "100%" : "534px"}
+                        direction="row"
+                      >
+                        {loading ? (
+                          <Spinner size="medium" />
+                        ) : error ? (
+                          <Text type="body" size="small" appearance="danger">
+                            {error}
+                          </Text>
+                        ) : (
+                          <Autosuggest
+                            name="employee"
+                            id="employee"
+                            placeholder="Escribe para buscar..."
+                            value={formik.values.employee}
+                            options={employeeOptions}
+                            onChange={(_name, newValue) => {
+                              formik.setFieldValue("employee", newValue);
+                            }}
+                            onBlur={() => {
+                              const isValid = employeeOptions.some(
+                                (opt) => opt.value === selectedOption,
+                              );
+                              if (!isValid) {
+                                formik.setFieldError(
+                                  "employee",
+                                  "Debes seleccionar una opción válida.",
+                                );
+                                setSelectedOption("");
+                                formik.setFieldValue("employee", "");
+                              } else {
+                                formik.setFieldValue(
+                                  "employee",
+                                  selectedOption,
+                                );
+                              }
+                            }}
+                            onFocus={() => {
+                              setSelectedOption(selectedOption);
+                            }}
+                            size="wide"
+                            fullwidth
+                          />
+                        )}
+
+                        <Button
+                          appearance="primary"
+                          spacing="wide"
+                          variant="filled"
+                          type="submit"
+                          disabled={!formik.isValid || !formik.dirty || loading}
+                          iconBefore={
+                            isMobile ? <MdOutlineArrowForward /> : undefined
+                          }
+                        >
+                          {!isMobile && "Continuar"}
+                        </Button>
+                      </Stack>
+
+                      {formik.errors.employee && formik.touched.employee && (
+                        <Text type="body" size="small" appearance="danger">
+                          {formik.errors.employee}
+                        </Text>
+                      )}
+                    </form>
+                  </StyledQuickAccessContainer>
+                </Stack>
+              </StyledQuickAccessContainer>
+            ) : (
+              <>
                 <Text type="headline" size="large">
                   Seleccionar empleado
                 </Text>
-
                 <Text type="body" size="large" appearance="gray">
-                  Digita la referencia o nombre del empleado que quieres
-                  seleccionar.
+                  Digita el nombre del empleado que quieres seleccionar.
                 </Text>
 
-                {loading && (
-                  <Text appearance="gray">Cargando empleados...</Text>
-                )}
-                {error && <Text appearance="danger">{error}</Text>}
+                <form onSubmit={formik.handleSubmit}>
+                  <Stack
+                    gap={spacing.s150}
+                    alignItems="center"
+                    width="100%"
+                    direction="row"
+                  >
+                    {loading ? (
+                      <Spinner size="medium" />
+                    ) : error ? (
+                      <Text type="body" size="small" appearance="danger">
+                        {error}
+                      </Text>
+                    ) : (
+                      <Autosuggest
+                        name="employee"
+                        id="employee"
+                        placeholder="Escribe para buscar..."
+                        value={formik.values.employee}
+                        options={employeeOptions}
+                        onChange={(_name, newValue) => {
+                          formik.setFieldValue("employee", newValue);
+                        }}
+                        onBlur={() => {
+                          const isValid = employeeOptions.some(
+                            (opt) => opt.value === selectedOption,
+                          );
+                          if (!isValid) {
+                            formik.setFieldError(
+                              "employee",
+                              "Debes seleccionar una opción válida.",
+                            );
+                            setSelectedOption("");
+                            formik.setFieldValue("employee", "");
+                          } else {
+                            formik.setFieldValue("employee", selectedOption);
+                          }
+                        }}
+                        onFocus={() => {
+                          setSelectedOption(selectedOption);
+                        }}
+                        size="wide"
+                        fullwidth
+                      />
+                    )}
+                    <Button
+                      appearance="primary"
+                      spacing="wide"
+                      variant="filled"
+                      type="submit"
+                      disabled={!formik.isValid || !formik.dirty || loading}
+                      iconBefore={
+                        isMobile ? <MdOutlineArrowForward /> : undefined
+                      }
+                    >
+                      {!isMobile && "Continuar"}
+                    </Button>
+                  </Stack>
 
-                <StyledQuickAccessContainer>
-                  <form onSubmit={formik.handleSubmit}>
-                    <Stack gap={spacing.s150} alignItems="center" width="534px">
-                      <div style={{ position: "relative", width: "100%" }}>
-                        <div style={{ position: "relative" }}>
-                          <Textfield
-                            placeholder="Palabra clave"
-                            name="keyword"
-                            id="names"
-                            size="compact"
-                            value={formik.values.keyword}
-                            onChange={(e) => {
-                              formik.handleChange(e);
-                              handleSearch(e.target.value);
-                            }}
-                            fullwidth
-                            onBlur={formik.handleBlur}
-                            status={
-                              formik.touched.keyword && formik.errors.keyword
-                                ? "invalid"
-                                : undefined
-                            }
-                            message={
-                              formik.touched.keyword && formik.errors.keyword
-                                ? formik.errors.keyword
-                                : undefined
-                            }
-                          />
-                          {formik.values.keyword && (
-                            <IoMdCloseCircleOutline
-                              style={{
-                                position: "absolute",
-                                top: "55%",
-                                right: "10px",
-                                transform: "translateY(-50%)",
-                                cursor: "pointer",
-                              }}
-                              onClick={() => {
-                                void formik.setFieldValue("keyword", "");
-                              }}
-                            />
-                          )}
-                        </div>
-                        {filteredEmployees.length > 0 && (
-                          <StyledDropdownMenu>
-                            {filteredEmployees.map((emp) => (
-                              <StyledDropdownItem
-                                key={emp.identificationDocumentNumber}
-                                onClick={() => {
-                                  formik.setFieldValue(
-                                    "keyword",
-                                    `${emp.identificationDocumentNumber} - ${emp.names}`,
-                                  );
-                                  setFilteredEmployees([]);
-                                  refetch();
-                                }}
-                              >
-                                <Text
-                                  size="large"
-                                  type="body"
-                                  appearance="gray"
-                                >
-                                  {emp.identificationDocumentNumber} -{" "}
-                                  {emp.names}
-                                </Text>
-                              </StyledDropdownItem>
-                            ))}
-                          </StyledDropdownMenu>
-                        )}
-                      </div>
-                      <Button
-                        appearance="primary"
-                        spacing="wide"
-                        variant="filled"
-                        type="submit"
-                        disabled={!formik.isValid || !formik.dirty}
-                      >
-                        Continuar
-                      </Button>
-                    </Stack>
-                  </form>
-                </StyledQuickAccessContainer>
-              </Stack>
-            </StyledQuickAccessContainer>
-            <Stack justifyContent="end">
+                  {formik.errors.employee && formik.touched.employee && (
+                    <Text type="body" size="small" appearance="danger">
+                      {formik.errors.employee}
+                    </Text>
+                  )}
+                </form>
+              </>
+            )}
+
+            <Stack width="100%" justifyContent="flex-end">
               <Button
                 appearance="primary"
                 iconBefore={<MdOutlineAdd />}
                 variant="none"
                 spacing="wide"
                 type="button"
-                onClick={() => {
-                  console.log("Redirigir a vincular nuevo empleado");
-                }}
+                onClick={() =>
+                  console.log("Redirigir a agregar nuevo empleado")
+                }
               >
-                Vincular nuevo empleado
+                {isMobile
+                  ? "Vincular nuevo empleado"
+                  : "Agregar nuevo empleado"}
               </Button>
             </Stack>
           </Stack>
--- src/pages/Employees/SelectEmployee/styles.ts
+++ src/pages/Employees/SelectEmployee/styles.ts
@@ -6,7 +6,11 @@ import { spacing } from "@design/tokens/spacing";
 const StyledAppPage = styled.div`
   display: flex;
   justify-content: center;
-  padding: ${spacing.s200};
+  padding: ${spacing.s1000};
+
+  @media (max-width: 700px) {
+    padding: ${spacing.s400};
+  }
 `;
 
 const StyledQuickAccessContainer = styled.div`
@@ -17,42 +21,4 @@ const StyledQuickAccessContainer = styled.div`
   border-radius: ${spacing.s100};
 `;
 
-const StyledDropdownMenu = styled.div`
-  position: absolute;
-  cursor: pointer;
-  left: 0;
-  width: 100%;
-  background: white;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  z-index: 10;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-`;
-
-const StyledDropdownItem = styled.div`
-  padding-top: 4px;
-  padding-bottom: 4px;
-  padding-left: 10px;
-  padding-right: 10px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${({ theme }) =>
-      theme?.palette?.neutral?.N30 || inube.palette.neutral.N30};
-    & > * {
-      color: ${({ theme }) =>
-        theme?.palette?.blue?.B400 || inube.palette.blue.B400};
-    }
-  }
-
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-`;
-
-export {
-  StyledAppPage,
-  StyledQuickAccessContainer,
-  StyledDropdownMenu,
-  StyledDropdownItem,
-};
+export { StyledAppPage, StyledQuickAccessContainer };
