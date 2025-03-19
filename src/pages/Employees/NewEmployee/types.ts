e5cc0a5c8db77c1b49d81305d363fb0a63aa73d8
Johan Nova <87447257+novadeveloper14@users.noreply.github.com>
2025-03-19 11:51:18 -0500

    erm15 Vinculación Empleado: Asistido Paso Verificación (#31)
    * feat(assignmentForm): employee onboarding assistant assignment details step
    
    * feat(verificationForm): employee onboarding assistant verification step
    
    * refactor: implement finish modals
    
    * fix: adjust imports

--- src/pages/Employees/NewEmployee/types.ts
+++ src/pages/Employees/NewEmployee/types.ts
@@ -1,7 +1,27 @@
+import { AlertCardProps } from "@components/data/AlertCard";
+
 import { IPersonalDataEntry } from "./forms/PersonalDataForm/types";
+import { IContractualPositionData } from "./forms/ContractualPositionDataForm/types";
+import { ILegalAccountingLocation } from "./forms/LegalAccountingLocationForm/types";
 
 export interface IFormsUpdateData {
   personalInformation: { isValid: boolean; values: IPersonalDataEntry };
+  contractualPositionData: {
+    isValid: boolean;
+    values: IContractualPositionData;
+  };
+  legalAccountingLocation: {
+    isValid: boolean;
+    values: ILegalAccountingLocation;
+  };
+  assignmentForm: {
+    isValid: boolean;
+    values: IAssignment[];
+  };
+  unmetRequirements: {
+    isValid: boolean;
+    values: AlertCardProps[];
+  };
 }
 
 export interface ModalState {
